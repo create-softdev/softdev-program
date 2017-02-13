d705b587e3872ffae59dfec36d2d4bcb670b5682
Gil Tayar <gil@tayar.org>
2017-02-13 21:33:42 +0200

    modifications based on fed-bootcamp experience

---
+++ modules/07-JS-Intro/praxis/7-more-exercises.js
@@ -0,0 +1,111 @@
+/**
+ * Function that returns true if the number is even or divides by 5
+ *
+ * isEvenOrDividesBy5(6) => true
+ * isEvenOrDividesBy5(10) => true
+ * isEvenOrDividesBy5(15) => true
+ * isEvenOrDividesBy5(7) => false
+ * isEvenOrDividesBy5(9) => false
+ *
+ * Test this function!
+ */
+function isEvenOrDividesBy5 (num) {
+}
+
+/**
+ * Write the function min that will return the minimum of the two parameters:
+ *
+ * min(4, 5) => 4
+ * min(6, 2) => 2
+ * min(3, 3) => 3
+ *
+ * Test this function!
+ */
+function min (a, b) {
+
+}
+
+/**
+ * Write the function max that will return the maximum of the two parameters:
+ *
+ * min(4, 5) => 5
+ * min(6, 2) => 6
+ * min(3, 3) => 3
+ *
+ * Test this function!
+ */
+function max (a, b) {
+
+}
+
+/**
+ * Write a function that will print a, b - sorted
+ *
+ * sort(4, 7) => prints 4 7
+ * sort(7, 4) => prints 4 7
+ *
+ * Use min and max that you wrote above for that
+ *
+ * Test this function!
+ */
+function sort (a, b) {
+
+}
+
+/**
+ * Check whether two players can play the game of thrones. True only iff both are adults (>= 18)
+ *
+ * canPlayGameOfThrones(7, 19) => false
+ * canPlayGameOfThrones(18, 19) => true
+ * canPlayGameOfThrones(8, 8) => false
+ *
+ * Test this function!
+ */
+function canPlayGameOfThrones (age1, age2) {
+
+}
+
+/**
+ * Check whether two players can play the cards against humanity game.
+ * True only iff at least one is an adult (>= 18)
+ *
+ * canPlayCardsAgainstHumanity(7, 19) => true
+ * canPlayCardsAgainstHumanity(19, 7) => true
+ * canPlayCardsAgainstHumanity(7, 8) => false
+ * canPlayCardsAgainstHumanity(20, 20) => true
+ *
+ * Test this function!
+ */
+function canPlayCardsAgainstHumanity (age1, age2) {
+
+}
+
+/**
+ * Check whether two players can play Barbie.
+ * True only iff none of them are aduly(>= 18)
+ *
+ * canPlayBarbie(7, 19) => false
+ * canPlayBarbie(18, 7) => false
+ * canPlayBarbie(7, 8) => true
+ * canPlayBarbie(20, 20) => false
+ *
+ * Test this function!
+ */
+function canPlayBarbie (age1, age2) {
+
+}
+
+/**
+ * Check whether two players can play a game. The games can be Barbie, CardsAgainstHumanity, GameOfThrones,
+ * or Monopoly. Monopoly, everyone can play. For the others, the rules are above. Please use the functions
+ * above to implement this function.
+ *
+ * canPlay('Monopoly', 4, 6) => true
+ * canPlay('Barbie', 10, 20) => false
+ * ...
+ *
+ * Test this function!
+ */
+function canPlay (game, age1, age2) {
+
+}
