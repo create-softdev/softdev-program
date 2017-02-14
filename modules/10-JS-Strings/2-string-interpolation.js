63adf66911c4181c3ca45f8db51c3f65572c9d93
Gil Tayar <gil@tayar.org>
2017-02-14 15:06:55 +0200

    Small tuneup to string interpolation slide

--- modules/10-JS-Strings/2-string-interpolation.js
+++ modules/10-JS-Strings/2-string-interpolation.js
@@ -1,5 +1,7 @@
 function greet (name) {
-  return `The name is ${name.split(' ')[1]}. ${name}.`
+  const firstAndLastName = name.split(' ')
+
+  return `The name is ${firstAndLastName[1]}. ${name}.`
 }
 
 console.log(greet('James Bond')) // The name is Bond. James Bond.
