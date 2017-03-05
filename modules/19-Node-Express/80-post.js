b66c62d27305a64f3cf9d259fcac2b56556543a8
Gil Tayar <gil@tayar.org>
2017-03-05 23:07:14 +0200

    more node-express module stuff

--- modules/19-Node-Express/80-post.js
+++ modules/19-Node-Express/80-post.js
@@ -6,7 +6,6 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/add', function (req, res) {
-  console.log(req.body)
   const a = parseInt(req.body.a)
   const b = parseInt(req.body.b)
 
@@ -18,7 +17,13 @@ app.listen(process.env.PORT || 3000, function () {
 })
 
 /*
+* This is how - a POST request handler.
+* A POST request handler looks like this (show the curl output)
+* The body looks like this: a=4&b=5 (just like in the query)
+* But the body can hold so much more text, so this is used when we want to send a LOT of data
+* But we need to parse the body, and express doesn't do that by itself.
+* We need to use "middleware", which parses the body.
+* body-parser and app.use. It's not important to understand the details.
+*
 * curl 'http://localhost:3000/add' -X POST -d 'a=4' -d 'b=5'
-* middleware
-* bodyParser.urlencoded
 */
---
+++ modules/19-Node-Express/90-post-json.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/add', function (req, res) {
+  console.log(req.body)
+  const a = parseInt(req.body.a)
+  const b = parseInt(req.body.b)
+
+  res.send(`<h1>The sum of ${a} + ${b} is ${a + b}</h1>`)
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening...')
+})
+
+/*
+* The body can be full JSON.
+* How can the browser know whether it's JSON or url-encoded?
+* Explain headers
+* Explain Content-Type
+* bodyParser.json
+* curl 'http://localhost:3000/add'  -H "Content-Type=application/json" -X POST -d '{"a":4,"b":5}' \
+
+* Can the browser send such a POST using <form>? No.
+* So, how?
+* We'll see later when we learn about Ajax.
+*/
