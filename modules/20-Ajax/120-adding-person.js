3b06023f09a81fb66208363a3c1e5b088e251b56
Gil Tayar <gil@tayar.org>
2017-03-09 11:17:03 +0200

    More in ajax module

---
+++ modules/20-Ajax/120-adding-person.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use('/static', express.static(__dirname))
+
+const people = [
+  { name: 'Danny', age: 33 },
+  { name: 'Sunny', age: 22 }
+]
+
+app.get('/people', (req, res) => {
+  res.json(people)
+})
+
+app.use(bodyParser.json())
+
+app.post('/delete-person', (req, res) => {
+  people.splice(req.body.index, 1)
+  res.send('')
+})
+
+app.post('/add-person', (req, res) => {
+  people.push({name: req.body.name, age: req.body.age})
+
+  res.send('')
+})
+
+
+app.listen(3000, function () {
+  console.log('Listening...')
+})
+
+/**
+ * Added a POST for add-person, which does exactly that.
+ */
